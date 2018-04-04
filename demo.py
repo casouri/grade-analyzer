2db2b369e08f2f0661169e13a125875b6db01742
Yuan Fu <casouri@gmail.com>
2018-04-04 17:19:41 -0400

    A demo to Canvas api
    Run this little cli to see the gradebook of a course.

---
+++ demo.py
@@ -0,0 +1,34 @@
+import json
+
+from server import CustomCanvas
+
+print('''
+Welcom to CustomCanvas demo.
+Enter number of the course to select that course,
+and hit Return to get the grade book of that course.
+''')
+
+# load config
+with open('server-config.json') as file1:
+    config = json.load(file1)
+
+canvas = CustomCanvas(config['api_url'], config['token'])
+while True:
+    # print courses
+    index = 0
+    for course in canvas.custom_get_course_string_list():
+        print('{} => {}'.format(index, course))
+        index += 1
+
+    seleced_course_index = int(input('select course: '))
+    course = canvas.course_list[seleced_course_index]
+
+    # show grade book
+    grade_book = canvas.custom_get_grade_of_course(course)
+    print(json.dumps(grade_book, sort_keys=True, indent=2))
+
+    keep_going = input('countinue? [y/N]: ')
+    if keep_going == 'y':
+        pass
+    else:
+        break
